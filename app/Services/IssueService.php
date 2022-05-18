0f0b9e553604288576588625ed3b40b4f2da83bb
Ash Allen <mail@ashallendesign.co.uk>
2022-05-18 23:45:12 +0100

    Code clean up.

--- app/Services/IssueService.php
+++ app/Services/IssueService.php
@@ -27,8 +27,8 @@ class IssueService
             $fetchedIssues = Cache::remember($url, now()->addMinutes(10), static fn () => Http::get($url)->json());
 
             foreach ($fetchedIssues as $fetchedIssue) {
-                if ($issue = $this->parseIssue($repo, $fetchedIssue)) {
-                    $issues[] = $issue;
+                if ($this->shouldIncludeIssue($fetchedIssue)) {
+                    $issues[] = $this->parseIssue($repo, $fetchedIssue);
                 }
             }
         }
@@ -38,14 +38,6 @@ class IssueService
 
     private function parseIssue(array $repo, array $fetchedIssue): ?Issue
     {
-        if (isset($fetchedIssue['pull_request'])) {
-            return null;
-        }
-
-        if (!$this->includesAtleastOneLabel($fetchedIssue, config('repos.labels'))) {
-            return null;
-        }
-
         $repoName = $repo['owner'].'/'.$repo['name'];
 
         $createdBy = new IssueOwner(
@@ -75,10 +67,21 @@ class IssueService
         );
     }
 
-    private function includesAtleastOneLabel(array $fetchedIssue, mixed $labels): bool
+    private function shouldIncludeIssue(array $fetchedIssue): bool
+    {
+        return ! $this->issueIsAPullRequest($fetchedIssue)
+            && $this->includesAtLeastOneLabel($fetchedIssue, config('repos.labels'));
+    }
+
+    private function includesAtLeastOneLabel(array $fetchedIssue, mixed $labels): bool
     {
         $issueLabels = Arr::pluck($fetchedIssue['labels'], 'name');
 
         return array_intersect($issueLabels, $labels) !== [];
+    }
+
+    private function issueIsAPullRequest(array $fetchedIssue): bool
+    {
+        return isset($fetchedIssue['pull_request']);
     }
 }
