07186cc79080d18f9772171a5a8a6843b64538c6
Ash Allen <mail@ashallendesign.co.uk>
2022-05-21 17:59:55 +0100

    Centered GitHub button.

--- resources/views/issues/index.blade.php
+++ resources/views/issues/index.blade.php
@@ -40,7 +40,7 @@
         <div class="flex flex-col sm:flex-row gap-2 justify-between item-center">
             <img class="w-48" src="/images/findapr.svg" alt="findapr.io logo">
             <a href="https://github.com/ash-jc-allen/find-a-pr"
-               class="flex items-center px-5 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+               class="flex justify-center items-center px-5 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                 <img src="/images/github-logo.png" class="w-6 pr-2 inline" alt="View on GitHub" />
                 View on GitHub
             </a>
