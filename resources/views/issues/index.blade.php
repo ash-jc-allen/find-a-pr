d2d41eb37b8ef69e243be6f8b5fa685934000f58
Ash Allen <mail@ashallendesign.co.uk>
2022-06-01 21:47:39 +0100

    Split issues into a card component.

--- resources/views/issues/index.blade.php
+++ resources/views/issues/index.blade.php
@@ -52,47 +52,7 @@
             </div>
 
             @foreach($issues as $issue)
-                <div class="border p-4 sm:py-5 sm:px-8 my-4 rounded-lg shadow bg-white">
-                    <div class="flex flex-col sm:flex-row justify-start sm:justify-between items-start gap-2">
-                        <div class="w-full md:w-3/4">
-                            <a href="{{ $issue->url }}" class="text-xl font-bold inline-block">{{ $issue->title }}</a>
-                            <div>
-                                <a href="{{ $issue->repoUrl }}" class="text-gray-400">{{ $issue->repoName }}</a>
-                            </div>
-                        </div>
-                        <div>
-                            <a href="{{ $issue->url }}"
-                               class="w-full inline-block px-5 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-400 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">View
-                                Issue</a>
-                        </div>
-                    </div>
-
-                    <div class="my-2">
-                        @foreach($issue->labels as $label)
-                            <span
-                                class="inline-flex items-center px-2 py-0.5 rounded text-xs font-bold border bg-opacity-20"
-                                style="color: {{ $label->color }}; border-color: {{ $label->color }}; background-color: {{ $label->color.'30' }}">
-                        {{ $label->name }}
-                    </span>
-                        @endforeach
-                    </div>
-
-                    <p class="my-4">
-                        {{ str($issue->body)->limit(150) }}
-                    </p>
-
-                    <div class="flex flex-col sm:flex-row gap-2 justify-between items-start sm:items-center">
-                        <a href="{{ $issue->createdBy->url }}"
-                           class="border hover:bg-gray-100 inline-block p-2 rounded">
-                            <img src="{{ $issue->createdBy->profilePictureUrl }}"
-                                 class="inline-block h-6 w-6 rounded-full" alt="{{ $issue->createdBy->name }}">
-                            <p class="inline">{{ $issue->createdBy->name }}</p>
-                        </a>
-
-                        <p class="text-sm text-gray-400">{{ $issue->createdAt->format('jS M Y @ H:i') }}</p>
-                    </div>
-
-                </div>
+                <x-issue-card :issue="$issue" />
             @endforeach
         </div>
 
