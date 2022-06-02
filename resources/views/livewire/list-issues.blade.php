636a6365d68a774159a0cae8fefc5184ec0a20e2
Ash Allen <mail@ashallendesign.co.uk>
2022-06-02 02:05:19 +0100

    Added empty state.

--- resources/views/livewire/list-issues.blade.php
+++ resources/views/livewire/list-issues.blade.php
@@ -8,8 +8,17 @@
             </p>
         </div>
 
-        @foreach($issues as $issue)
+        @forelse($issues as $issue)
             <x-issue-card :issue="$issue"/>
-        @endforeach
+        @empty
+            <div class="my-14 text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-24 text-gray-300 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+
+                <p class="font-medium my-4">No Issues Found!</p>
+                <p>It looks there aren't any issues that fit your criteria.</p>
+            </div>
+        @endforelse
     </main>
 </div>
