a168452c21b95e0aa5648cae089bcd1769212046
jaetoole <jtooleuk@gmail.com>
2022-08-01 16:36:33 +0100

    Yet more style updates

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -3,8 +3,8 @@
 return [
     'name' => 'LaravelPWA',
     'manifest' => [
-        'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'name' => env('APP_NAME', 'findapr'),
+        'short_name' => 'FPR',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
@@ -59,20 +59,15 @@ return [
         ],
         'shortcuts' => [
             [
-                'name' => 'Shortcut Link 1',
-                'description' => 'Shortcut Link 1 Description',
-                'url' => '/shortcutlink1',
+                'name' => 'Home',
+                'description' => 'findapr.io Home',
+                'url' => '/',
                 'icons' => [
                     'src' => '/images/icons/icon-72x72.png',
-                    'purpose' => 'any'
+                    'purpose' => 'any',
                 ]
             ],
-            [
-                'name' => 'Shortcut Link 2',
-                'description' => 'Shortcut Link 2 Description',
-                'url' => '/shortcutlink2'
-            ]
         ],
-        'custom' => []
+        'custom' => [],
     ]
 ];
