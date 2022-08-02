503fce2cba199a64338813ef5a93ce2144d34130
Ruben van Erk <ruben@rubenvanerk.nl>
2022-08-02 21:22:59 +0200

    Add model and migration for SocialPost

---
+++ database/migrations/2022_08_02_191635_create_social_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('issue_repo');
+            $table->integer('issue_number')->unsigned();
+            $table->bigInteger('issue_id')->unsigned();
+            $table->string('twitter_url');
+            $table->timestamp('twitter_sent_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_posts');
+    }
+};
