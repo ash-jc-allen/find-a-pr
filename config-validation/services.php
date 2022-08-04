a4ecfa88497f5e3c791663ca3ce35f98de3166c0
Ash Allen <mail@ashallendesign.co.uk>
2022-08-04 18:26:46 +0100

    Added validation for Oh Dear and Bugsnag config fields.

--- config-validation/services.php
+++ config-validation/services.php
@@ -3,21 +3,9 @@
 use AshAllenDesign\ConfigValidator\Services\Rule;
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Local
-    |--------------------------------------------------------------------------
-    */
-    Rule::make('github.username')->rules(['string', 'nullable'])->environments(['local']),
-
-    Rule::make('github.token')->rules(['string', 'nullable'])->environments(['local']),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Production
-    |--------------------------------------------------------------------------
-    */
     Rule::make('github.username')->rules(['string', 'required'])->environments(['production']),
 
     Rule::make('github.token')->rules(['string', 'required'])->environments(['production']),
+
+    Rule::make('ohdear.ping_url')->rules(['url', 'required'])->environments(['production']),
 ];
