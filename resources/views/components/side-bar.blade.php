0f930d8e83b98bde11dc1f09fd5314cf74489907
Jeroen Custers <jeroen@hobbyshop-online.nl>
2022-08-15 13:56:32 +0200

    Firefox support + search key hint

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -12,7 +12,7 @@
            x-ref="search"
            wire:model.debounce="searchTerm"
            type="search"
-           placeholder="Search..."
+           placeholder="Search... (Press '/')"
            class="w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-500 dark:bg-slate-900 dark:border-slate-600 dark:focus:ring-offset-slate-700 dark:text-gray-100"
     >
 
