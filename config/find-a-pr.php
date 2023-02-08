15a9fa0954823b491f5ff6ace4bc46719d651a8b
Ash Allen <mail@ashallendesign.co.uk>
2023-02-08 22:50:01 +0000

    Added a config variable to toggle whether to automatically tweet.

---
+++ config/find-a-pr.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tweet Issues
+    |--------------------------------------------------------------------------
+    |
+    | Toggle whether issues should be automatically posted to Twitter or not.
+    |
+    */
+
+    'tweet_issues' => env('TWEET_ISSUES', false),
+];
