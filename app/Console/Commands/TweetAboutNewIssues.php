d60d74cb12c1da992ebd5f56b5303a45c347066b
Ash Allen <mail@ashallendesign.co.uk>
2023-10-06 15:48:26 +0100

    Style updates.

--- app/Console/Commands/TweetAboutNewIssues.php
+++ app/Console/Commands/TweetAboutNewIssues.php
@@ -8,6 +8,7 @@ use App\Models\SocialPost;
 use App\Services\IssueService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
+
 use function Termwind\terminal;
 
 class TweetAboutNewIssues extends Command
