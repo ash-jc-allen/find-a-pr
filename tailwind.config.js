d485be35e655c6446ecc52d66588708cf5606581
Shift <shift@laravelshift.com>
2023-10-07 14:35:39 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     darkMode: 'class',
-
     content: ['./resources/**/*.blade.php'],
-    safelist: process.env.NODE_ENV === 'development' ? [{ pattern: /.*/ }] : [],
+    safelist:
+        process.env.NODE_ENV === 'development'
+            ? [
+                  {
+                      pattern: /.*/,
+                  },
+              ]
+            : [],
     plugins: [require('@tailwindcss/typography')],
-};
+}
