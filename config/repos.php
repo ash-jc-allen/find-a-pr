4ce443f9bc6f6d0dc0b0c0d16a3d320aab708d28
Gurpreet Kait <100063447+gurpreetkaits@users.noreply.github.com>
2025-12-30 12:01:05 +0530

    Add 'screensense' repository for gurpreetkaits
    Add new repository 'screensense' for user 'gurpreetkaits'.

--- config/repos.php
+++ config/repos.php
@@ -7,6 +7,9 @@ return [
         'cachethq',
     ],
     'repos' => [
+        'gurpreetkaits' => [
+            'screensense'
+        ],
         'PapaRascal2020' => [
             'sidekick',
         ],
