c8c38610067aa506070971566ac83fe45e67b258
Ash Allen <mail@ashallendesign.co.uk>
2026-06-27 13:27:29 +0100

    Updated `abraham/twitteroauth` from `^6.0` to `^7.0`.

--- app/Services/Twitter/OAuthTwitter.php
+++ app/Services/Twitter/OAuthTwitter.php
@@ -11,11 +11,11 @@ final readonly class OAuthTwitter implements TwitterInterface
     public function __construct(
         protected TwitterOAuth $twitter,
     ) {
-        $this->twitter->setApiVersion('2');
+        //
     }
 
     public function tweet(string $status): ?array
     {
-        return (array) $this->twitter->post('tweets', ['text' => $status], true);
+        return (array) $this->twitter->post('tweets', ['text' => $status], ['jsonPayload' => true]);
     }
 }
